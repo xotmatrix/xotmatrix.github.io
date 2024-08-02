dcf820c43f6ea8d75b9bd74705af2cdaf71dd9b3
xotmatrix <xotmatrix@gmail.com>
2024-08-02 02:16:13 -0400

    test response headers

---
+++ test/test.js
@@ -0,0 +1,22 @@
+fetch('/index.md')
+  .then(response => {
+    /*
+    // Get the value of the 'Content-Type' header
+    const contentType = response.headers.get('Content-Type');
+    console.log('Content-Type:', contentType);
+
+    // Get all the headers as a Headers object
+    const allHeaders = response.headers;
+    console.log('All headers:', allHeaders);
+
+    // Convert the Headers object to a plain JavaScript object
+    const headersObject = Object.fromEntries(allHeaders.entries());
+    console.log('Headers as an object:', headersObject);
+
+    // Access other properties of the response object
+    console.log('Status:', response.status);
+    console.log('OK:', response.ok);
+    */
+    document.write(response.headers.get('Date'));
+  })
+  .catch(error => console.error('Error:', error));
